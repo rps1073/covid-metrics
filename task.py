65d1282e64b20924fcca5cc6b02d69b290d6835e
Sheel <rps1073@gmail.com>
2022-07-24 13:12:34 -0400

    can insert to db

--- task.py
+++ task.py
@@ -1,15 +1,26 @@
+import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine
 from model.cases import cases
 
 print('Starting task')
 
 try:
-    covid_cases = pd.read_csv(
-        'https://raw.githubusercontent.com/biobotanalytics/covid19-wastewater-data/master/cases_by_county.csv')
+    conn_string = 'postgresql://postgres:password@localhost/covid_metrics'
+    db = create_engine(conn_string)
+    conn = db.connect()
+
+    covid_cases = pd.read_csv('https://raw.githubusercontent.com/biobotanalytics/covid19-wastewater-data/master/cases_by_county.csv')
     cases_MA = covid_cases[covid_cases['state'] == 'MA']
     cases_MA_formatted = cases(cases_MA)
-    print(cases_MA_formatted)
+
+    cases_MA_formatted.to_sql('daily_cases_average', conn, schema='wastewater', if_exists='replace', index=False)
+
+    conn.close()
 except Exception as e:
     print(e)
 
 print('Task complete')
+
+    # df = pd.read_sql('SELECT * FROM wastewater.daily_cases_average;', con = conn_string)
+    # print(df)
