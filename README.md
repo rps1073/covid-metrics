e25291f56c0c2e86da50f70514f8bc4d4f833214
Rushil Shah <rps1073@gmail.com>
2022-09-14 15:59:15 -0400

    can push to aws-hosted db

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # covid-metrics
 
-### Create virtual env folder
-`python -m venv project-name\venv`
+### Create virtual env folder named "venv" in project directory
+`virtualenv venv`
 
 ### Activate virtual env
-`. venv\scripts\activate`
+`source venv/bin/activate`
 
-### Install packages from requirements.txt (actually not sure what this does)
+### Install packages from requirements.txt
 `pip install -r requirements.txt`
 
-### Generate requirements.txt
-`pip freeze > requirements.txt`
+### Generate requirements.txt for brand new project
+`pip freeze --local > requirements.txt`
 
 ### Install package in virtual env and add it to requirements.txt
-`pip install psycopg2; pip freeze > requirements.txt`
+`pip install psycopg2; pip freeze --local > requirements.txt`
 
 ### De-activate virtual env
 `deactivate`
--- task.py
+++ task.py
@@ -1,18 +1,27 @@
 import psycopg2
 import pandas as pd
+import os
+from decouple import config
 from sqlalchemy import create_engine
 from model.cases import cases
 
 print('Starting task')
+db_config = {
+    "user": config('username'),
+    "password": config('password'),
+    "host": config('host'),
+    "database": config('database'),
+}
 
 try:
-    conn_string = 'postgresql://postgres:password@localhost/covid_metrics'
+    conn_string = f'postgresql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["database"]}'
     db = create_engine(conn_string)
     conn = db.connect()
 
     covid_cases = pd.read_csv('https://raw.githubusercontent.com/biobotanalytics/covid19-wastewater-data/master/cases_by_county.csv')
     cases_MA = covid_cases[covid_cases['state'] == 'MA']
     cases_MA_formatted = cases(cases_MA)
+    print(cases_MA_formatted)
 
     cases_MA_formatted.to_sql('daily_cases_average', conn, schema='wastewater', if_exists='replace', index=False)
 
