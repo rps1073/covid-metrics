0c8faad05acd9b7f101622d69dcdebfa453095e2
Rushil Shah <rps1073@gmail.com>
2023-03-28 21:24:12 -0400

    change back to aws postgres db

--- task.py
+++ task.py
@@ -14,18 +14,18 @@ db_config = {
 }
 
 try:
-    # conn_string = f'postgresql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["database"]}'
-    # db = create_engine(conn_string)
-    # conn = db.connect()
+    conn_string = f'postgresql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["database"]}'
+    db = create_engine(conn_string)
+    conn = db.connect()
 
-    covid_cases = pd.read_csv('https://raw.githubusercontent.com/biobotanalytics/covid19-wastewater-data/master/cases_by_county.csv')
+    covid_cases = pd.read_csv('https://raw.githubusercontent.com/biobotanalytics/covid19-wastewater-data/master/cases_by_county.csv', index_col=[0])
     # cases_MA = covid_cases[covid_cases['state'] == 'MA']
     # cases_MA_formatted = cases(cases_MA)
     print(covid_cases)
 
-    # cases_MA_formatted.to_sql('daily_cases_average', conn, schema='wastewater', if_exists='replace', index=False)
+    covid_cases.to_sql('daily_cases_average', conn, schema='covid_metrics_raw', if_exists='replace', index=False)
 
-    # conn.close()
+    conn.close()
 except Exception as e:
     print(e)
 
